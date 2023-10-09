a83db2210550f09b93dbdbbafab9e6ae66ac5e80
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2023-10-08 21:42:19 -0700

    Initial Commit

---
+++ GymGenius/GymGeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymGeniusApp.swift
+//  GymGenius
+//
+//  Created by Ishaan Bhasin on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GymGeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
